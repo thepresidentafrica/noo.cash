7fcb790f827110916948afefaa227fe0219757d2
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-07 21:52:38 +0200

    feat: initiate appRoutes The changes made in this commit include: - Added `appRoutes.tsx` file. - Modified `appRoutes.tsx` to define the `appRoutes` object. - Imported `DesktopRoutes`, `MobileRoutes`, and `WebsiteRoutes` from the current directory.
    Here is the summary of the git diff:
    
    ```diff
    diff --git a/application/src/routes/appRoutes.tsx b/application/src/routes/appRoutes.tsx
    new file mode 100644
    index 0000000..140fdf3
    --- /dev/null
    +++ b/application/src/routes/appRoutes.tsx
    @@ -0,0 +1,8 @@
    +import { DesktopRoutes, MobileRoutes, WebsiteRoutes } from ".";
    +
    +
    +export const appRoutes: {[key: string]: any} = {
    +    "mobile": MobileRoutes,
    +    "desktop": DesktopRoutes,
    +    "website": WebsiteRoutes
    +}
    \ No newline at end of file
    ```
    
    In this commit, a new file named `appRoutes.tsx` was added to the `application/src/routes` directory. The file defines an object named `appRoutes` which contains routes for different platforms such as "mobile", "desktop", and "website". The routes for each platform are imported from other files in the same directory.
    
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ application/src/routes/appRoutes.tsx
@@ -0,0 +1,8 @@
+import { DesktopRoutes, MobileRoutes, WebsiteRoutes } from ".";
+
+
+export const appRoutes: {[key: string]: any} = {
+    "mobile": MobileRoutes,
+    "desktop": DesktopRoutes,
+    "website": WebsiteRoutes
+}
