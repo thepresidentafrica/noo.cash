544d2924b17ad6b00549ee4c081b26fd94a032d7
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-08 22:20:27 +0200

    feat: add meta tags
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -2,10 +2,41 @@ import { RouterProvider } from "react-router-dom"
 import { env, nooCashClient } from "./utils"
 import { appRoutes } from "./routes"
 import { ApolloProvider } from "@apollo/client"
+import { Helmet } from "react-helmet"
+import { StructuredData } from "./components"
 export const Noocash = () => {
   return (
     <>
       <ApolloProvider client={nooCashClient}>
+        <div>
+          {/* Other content of your component */}
+          <StructuredData />
+          {/* Set the meta tags for SEO and social sharing */}
+          <Helmet>
+            {/* Standard meta tags for SEO */}
+            <title className="capitalize">Noo.Cash | {env.VITE_APP_MODE}</title>
+            <meta name="description" content="Your trusted online payment platform for secure and hassle-free digital transactions. Join the cashless revolution with Noo.Cash." />
+            <meta name="keywords" content="Noo.Cash, online payments, secure transactions, digital payments, cashless payments" />
+
+            {/* Open Graph (OG) tags for social sharing */}
+            <meta property="og:title" content="Noo.Cash - Your Cashless Payment Platform" />
+            <meta property="og:description" content="Your trusted online payment platform for secure and hassle-free digital transactions. Join the cashless revolution with Noo.Cash." />
+            <meta property="og:image" content="https://www.noo.cash/social/noocash-social.png" />
+            <meta property="og:url" content="https://www.noo.cash" />
+            <meta property="og:type" content="website" />
+            <meta property="og:site_name" content="Noo.Cash" />
+
+            {/* Twitter Card tags for Twitter sharing */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@NooCash" />
+
+            <meta name="author" content="noo.cash" />
+            <meta name="robots" content="index, follow" />
+            <link rel="canonical" href="https://www.noo.cash" />
+
+            {/* Add other meta tags as needed for your specific requirements */}
+          </Helmet>
+        </div>
         <RouterProvider router={appRoutes[env.VITE_APP_MODE]} />
       </ApolloProvider>
     </>
---
+++ application/src/components/SEO/StructuredData.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet';
+
+export const StructuredData = () => {
+  const structuredData = {
+    "@context": "http://schema.org",
+    "@type": "WebSite",
+    "url": "https://www.noo.cash",
+    "name": "Noo.Cash",
+    "description": "Your trusted online payment platform for secure and hassle-free digital transactions. Join the cashless revolution with Noo.Cash.",
+    "potentialAction": {
+      "@type": "SearchAction",
+      "target": "https://www.noo.cash/search?q={search_term_string}",
+      "query-input": "required name=search_term_string"
+    }
+  };
+
+  return (
+    <Helmet>
+      <script type="application/ld+json">{JSON.stringify(structuredData)}</script>
+    </Helmet>
+  );
+};
+
+export default StructuredData;
