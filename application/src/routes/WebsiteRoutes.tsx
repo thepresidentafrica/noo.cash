1eb78d0580ca325fa06e3c5defcadc4f67b5a532
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-15 07:52:38 +0200

    feat: add website config
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/src/routes/WebsiteRoutes.tsx
+++ application/src/routes/WebsiteRoutes.tsx
@@ -1,8 +1,10 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {WebsiteNavigation} from "../components";
+import { WebsiteHomePage } from "../pages";
 export const WebsiteRoutes= createBrowserRouter(
     createRoutesFromElements(
-        <Route>
-            <Route path="/" element={<div>Website Application</div>}/>
+        <Route element={<WebsiteNavigation/>}>
+            <Route path="/" element={<WebsiteHomePage/>}/>
         </Route>
     )
 )
