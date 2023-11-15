272d85b508349177afe5fb75a28b25e83c2ab0e0
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-15 06:40:39 +0200

    feat: add logo
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ application/src/components/Logo/Logo.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react"
+import { NavLink } from "react-router-dom"
+
+export const Logo: FC<{ text?: string }> = ({ text }) => {
+    return (
+        <>
+            <NavLink to="/#home" className='flex justify-start items-center space-x-3'>
+                <span className="sr-only">noo.cash</span>
+                <img
+                    className="h-8 w-auto sm:h-10"
+                    src="/logo.svg"
+                    alt=""
+                />
+                {text && <p className='sm:text-4xl text-2xl text-nocash'>{text}</p>}
+            </NavLink>
+        </>
+    )
+}
