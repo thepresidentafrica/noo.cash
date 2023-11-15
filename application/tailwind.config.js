5bca239ace396a1c684306bd2125cc727310c331
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-15 07:52:51 +0200

    feat: tailwind adjustments
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/tailwind.config.js
+++ application/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./**/*.{tsx,html,scss}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nocash': {  DEFAULT: '#AD13AF',  50: '#FAD1FA',  100: '#F7BFF8',  200: '#F39AF4',  300: '#EE75F0',  400: '#EA50EC',  500: '#E62CE8',  600: '#D117D4',  700: '#AD13AF',  800: '#7B0E7C',  900: '#49084A',  950: '#300530'},
+      }
+    },
   },
   plugins: [],
 }
