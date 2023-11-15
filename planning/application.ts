a3a118ca940289c8c33ca307a6d884996b05341e
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-15 13:36:53 +0200

    feat: add app code track file
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ planning/application.ts
@@ -0,0 +1,10 @@
+/** 
+ * TODO: Enable Payments with QR Code Scans or PIN
+
+**Description:** Allow users to make payments in any merchant shop or online using QR code scans or PIN entry.
+
+**Subtasks:**
+1. Implement QR code scanning functionality.
+2. Develop a secure PIN entry system for payments.
+3. Integrate payment processing logic. 
+*/
