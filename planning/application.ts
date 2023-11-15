b4e77ccfabba485bab903c7852233c1b96c540ec
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-15 13:42:34 +0200

    chore: add issues for core app
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- planning/application.ts
+++ planning/application.ts
@@ -8,3 +8,39 @@
 2. Develop a secure PIN entry system for payments.
 3. Integrate payment processing logic. 
 */
+/**
+ * TODO: Enable Payments with QR Code Scans or PIN
+ *
+ * Description: Allow users to make payments in any merchant shop or online using QR code scans or PIN entry.
+ *
+ * Subtasks:
+ * 1. Implement QR code scanning functionality.
+ * 2. Develop a secure PIN entry system for payments.
+ * 3. Integrate payment processing logic.
+ */
+
+// 1. Implement QR code scanning functionality.
+function scanQRCode(): void {
+  // Code to scan QR code goes here
+}
+
+// 2. Develop a secure PIN entry system for payments.
+function enterPIN(): void {
+  // Code to enter PIN goes here
+}
+
+// 3. Integrate payment processing logic.
+function processPayment(): void {
+  // Code to process payment goes here
+}
+/**
+ * 
+ * Issue : Priority Feature - Enable Airtime Payments
+
+**Description:** Prioritize the development of the core feature where users can make payments using airtime on their phones.
+
+**Subtasks:**
+1. Research and integrate airtime payment APIs.
+2. Implement a seamless airtime payment process.
+3. Ensure robust error handling for airtime payments.
+ */
