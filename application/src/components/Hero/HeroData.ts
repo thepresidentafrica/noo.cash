0dc4afafd77eba8eaf4fe43825d65ca69ac6a9a1
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-25 07:13:59 +0200

    feat: add new hero sections and fonts
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ application/src/components/Hero/HeroData.ts
@@ -0,0 +1,13 @@
+export const HERO_DATA = {
+    title: 'Experience the Future of Payments',
+    description:
+      'Make instant cashless payments at any store or online. noo.cash revolutionizes the way you pay, providing a seamless and secure experience without the need for physical cash.',
+    cta: {
+      label: 'Get Started',
+      url: '/get-started',
+    },
+    image: {
+      src: '/social/noocash-social.png',
+      alt: 'Payment Image',
+    },
+  };
--- application/src/components/Hero/index.ts
+++ application/src/components/Hero/index.ts
@@ -1,1 +1,2 @@
+export * from './heroData';
 export * from './WebsiteHero';
