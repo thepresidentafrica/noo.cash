cebc1585139339214afa758bb9faca87aaa61e85
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-25 07:15:13 +0200

    feat: add error page
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ application/src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,10 @@
+export const ErrorPage = () => {
+    return (
+        <div className="h-screen min-h-screen maxi-h-screen bg-nocash-100 flex justify-center items-center">
+            <div className="text-image">
+                <h1>There seem to be a problem accessing the page</h1>
+                <p>Please try again later</p>
+            </div>
+        </div>
+    )
+}
