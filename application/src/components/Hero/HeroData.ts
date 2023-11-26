92c8296a54ef38b8cce3eb520dad476b011c007a
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-26 06:25:13 +0200

    feat: update get- started url The changes made in this commit include: - Modified HeroData.ts to update the URL for the CTA button in the HERO_DATA object. The URL was changed from '/get-started' to '/auth/register'.
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/src/components/Hero/HeroData.ts
+++ application/src/components/Hero/HeroData.ts
@@ -4,7 +4,7 @@ export const HERO_DATA = {
       'Make instant cashless payments at any store or online. noo.cash revolutionizes the way you pay, providing a seamless and secure experience without the need for physical cash.',
     cta: {
       label: 'Get Started',
-      url: '/get-started',
+      url: '/auth/register',
     },
     image: {
       src: '/social/noocash-social.png',
