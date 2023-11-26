0b169a76e712a73cb4a3da9adeed65963bbd20cf
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-26 05:55:58 +0200

    feat: update heroData The changes made in this commit include: - Added features to the `HERO_DATA` object in `HeroData.ts` file. - Modified the `HERO_DATA` object in `HeroData.ts` file to include new features. - No files were deleted in this commit.
    Here is a summary of the git diff:
    
    ```diff
    diff --git a/application/src/components/Hero/HeroData.ts b/application/src/components/Hero/HeroData.ts
    index ad34554..46d2d99 100644
    --- a/application/src/components/Hero/HeroData.ts
    +++ b/application/src/components/Hero/HeroData.ts
    @@ -10,4 +10,18 @@ export const HERO_DATA = {
           src: '/social/noocash-social.png',
           alt: 'Payment Image',
         },
    +    features: [
    +      {
    +        title: 'Airtime-Powered Transactions:',
    +        description:`Noo.Cash introduces a groundbreaking feature that allows you to pay with your phone's airtime. Say goodbye to traditional payment methods and embrace the simplicity and innovation of paying directly with your mobile balance. This unique functionality sets Noo.Cash apart, providing users with a convenient and novel way to make transactions. Embrace the future of mobile payments with Noo.Cash.`
    +      },
    +      {
    +        title: 'Smart Savings and Rewards:',
    +        description: `Noo.Cash goes beyond ordinary transactions – it's about earning as you spend. Our smart savings feature lets users receive small amounts of money with each transaction. Think of it as a rewards system or a gamified feature that encourages you to save while you spend. Experience the joy of watching your savings grow effortlessly with every purchase. Noo.Cash not only transforms payments but also turns spending into a rewarding experience.`
    +      },
    +      {
    +        title: 'Instant Cashless Payments:',
    +        description: `Discover a revolutionary way to make payments with Noo.Cash. Enjoy the future of payments with instant and secure cashless transactions, whether you're shopping in-store or online. Experience unparalleled convenience and speed, eliminating the need for physical cash. Noo.Cash transforms the way you pay, making every transaction a seamless and futuristic experience.`
    +      }
    +    ]
       };
    \ No newline at end of file
    ```
    
    These changes add new features to the `HERO_DATA` object in the `HeroData.ts` file. The features include "Airtime-Powered Transactions," "Smart Savings and Rewards," and "Instant Cashless Payments." Each feature has a title and a description explaining its functionality and benefits.
    
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/src/components/Hero/HeroData.ts
+++ application/src/components/Hero/HeroData.ts
@@ -10,4 +10,18 @@ export const HERO_DATA = {
       src: '/social/noocash-social.png',
       alt: 'Payment Image',
     },
+    features: [
+      {
+        title: 'Airtime-Powered Transactions:',
+        description:`Noo.Cash introduces a groundbreaking feature that allows you to pay with your phone's airtime. Say goodbye to traditional payment methods and embrace the simplicity and innovation of paying directly with your mobile balance. This unique functionality sets Noo.Cash apart, providing users with a convenient and novel way to make transactions. Embrace the future of mobile payments with Noo.Cash.`
+      },
+      {
+        title: 'Smart Savings and Rewards:',
+        description: `Noo.Cash goes beyond ordinary transactions – it's about earning as you spend. Our smart savings feature lets users receive small amounts of money with each transaction. Think of it as a rewards system or a gamified feature that encourages you to save while you spend. Experience the joy of watching your savings grow effortlessly with every purchase. Noo.Cash not only transforms payments but also turns spending into a rewarding experience.`
+      },
+      {
+        title: 'Instant Cashless Payments:',
+        description: `Discover a revolutionary way to make payments with Noo.Cash. Enjoy the future of payments with instant and secure cashless transactions, whether you're shopping in-store or online. Experience unparalleled convenience and speed, eliminating the need for physical cash. Noo.Cash transforms the way you pay, making every transaction a seamless and futuristic experience.`
+      }
+    ]
   };
