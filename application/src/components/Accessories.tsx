50d5b4a83c1e0a857f2768c63bad2dede11f21d6
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-11-27 09:16:02 +0200

    feat: add accesories
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- application/src/components/Accessories.tsx
+++ application/src/components/Accessories.tsx
@@ -1,36 +1,27 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
 export const AccessoriesComponent = () => {
 
     return (
         <div>
-            <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+            <ul className="space-y-4 ml-4 text-left text-gray-500 dark:text-gray-400">
                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                    </svg>
+                    <CheckIcon className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400"  />
                     <span>AI Powered Shopping Carts</span>
                 </li>
                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                    </svg>
+                    <CheckIcon className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400"  />
                     <span>Agregate Rewards Voucher Cards</span>
                 </li>
                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                    </svg>
+                    <CheckIcon className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400"  />
                     <span>Smart POS: <span className="font-semibold text-gray-900 dark:text-white">For Service Providers</span></span>
                 </li>
                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                    </svg>
+                    <CheckIcon className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400"  />
                     <span>Smart Wallet: <span className="font-semibold text-gray-900 dark:text-white">more than 10 supported currencies</span></span>
                 </li>
                 <li className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                    </svg>
+                    <CheckIcon className="flex-shrink-0 w-3.5 h-3.5 text-nocash-500 dark:text-nocash-400"  />
                     <span>Access Cards: <span className="font-semibold text-gray-900 dark:text-white">For smart Shops and event premises</span></span>
                 </li>
             </ul>
