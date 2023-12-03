0c90f1a1fa5a034e60a740d92e10fe4595ee05ca
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-12-03 14:04:05 +0200

    initial commit
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

---
+++ noocash-spring/src/test/java/cash/noo/app/noocash/NoocashApplicationTests.java
@@ -0,0 +1,13 @@
+package cash.noo.app.noocash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoocashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
