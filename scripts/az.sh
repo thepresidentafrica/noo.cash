583d8bae635aabf20734cfcb16bb4422e7d6ad6b
Mitch Chimwemwe chanza <mitch@opherlabs.co.za>
2023-12-03 14:30:06 +0200

    feat: update
    Signed-off-by: Mitch Chimwemwe chanza <mitch@opherlabs.co.za>

--- scripts/az.sh
+++ scripts/az.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 az ad sp create-for-rbac \
-  --name sombatout --role contributor \
+  --name sombatout-dev --role contributor \
   --scopes /subscriptions/74b12dcc-bd39-498b-91c0-6ad108eed2d1/resourceGroups/sombatout \
   --sdk-auth
--- sombatout/layouts/default.vue
+++ sombatout/layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <Navigation/>
+        <!-- Compare this snippet from sombatout/components/Navigation.vue: -->
         <slot/>
         <Footer />
     </div>
