412bb89bbd84696f498b86dfe22f0e0237831502
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-08 15:35:43 +0200

    feat: add sombatout version

--- scripts/az.sh
+++ scripts/az.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 az ad sp create-for-rbac \
-  --name enginear-dev --role contributor \
-  --scopes /subscriptions/74b12dcc-bd39-498b-91c0-6ad108eed2d1/resourceGroups/enginear \
+  --name sombatout-dev --role contributor \
+  --scopes /subscriptions/74b12dcc-bd39-498b-91c0-6ad108eed2d1/resourceGroups/sombatout-env \
   --sdk-auth
