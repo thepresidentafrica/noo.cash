b3165346133db47f3e11a9535afc744a585f46d5
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-28 14:12:50 +0200

    🛠️ WebsiteRoutes.tsx -> Updated import statements and added new route for ToolsPage

--- application/src/routes/WebsiteRoutes.tsx
+++ application/src/routes/WebsiteRoutes.tsx
@@ -1,5 +1,5 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import { AuthPage, WebsiteHomePage } from "../pages";
+import { AuthPage, ToolsPage, WebsiteHomePage } from "../pages";
 import { WebsitePagelayout } from "../Layouts";
 import { GeneralRoutes } from "./GeneralRoutes";
 export const WebsiteRoutes = createBrowserRouter(
@@ -8,6 +8,12 @@ export const WebsiteRoutes = createBrowserRouter(
             <Route element={<WebsitePagelayout />}> 
                 <Route index path="/" element={<WebsiteHomePage />} />
                 
+            </Route>
+            <Route>
+                <Route path="/t" >
+                    <Route index  path="/t" element={<ToolsPage />} />
+                    <Route path="/t/:option" element={<ToolsPage />} />
+                </Route>
             </Route>
             <Route path="/auth">
                 <Route index path="/auth" element={<AuthPage />} />
