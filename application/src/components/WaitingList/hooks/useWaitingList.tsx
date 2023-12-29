008601640f01ce3297dbb1306dfecf14a7761032
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 09:43:52 +0200

    🟢 useWaitingList.tsx - New file for managing waiting list functionality.

---
+++ application/src/components/WaitingList/hooks/useWaitingList.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { IWaitingList } from "../IWaitingList"
+import { useLazyQuery, useMutation } from "@apollo/client";
+import { JOIN_WAITING_LIST ,GET_WAITING_LIST} from "..";
+
+export const useWaitingList = (waitListData?: IWaitingList) => {
+    const [data, setData] = useState<{ list: IWaitingList| IWaitingList[], error: any } | null>(null);
+    const [joinWaitingList] = useMutation(JOIN_WAITING_LIST);
+    const [getWaitingList]= useLazyQuery(GET_WAITING_LIST);
+    useEffect(() => {
+        const waitList = async () => {
+            if (waitListData) {
+                const { data: result, errors } = await joinWaitingList({
+                    variables: {
+                        email: waitListData?.email,
+                        fullName: waitListData?.fullName,
+                        product: waitListData?.product,
+                        company: waitListData?.company
+                    }
+                })
+                if (result) {
+                    setData({ list: waitListData, error: errors })
+                }
+            } else {
+                const { data: result,error } = await getWaitingList();
+                if (result) {
+                    setData({ list: result.noocash_WailtingList, error })
+                }
+            }
+        }
+        waitList().then()
+    })
+    return data
+}
+
