208997bf300b945fadd7d1dd9bb778ec28d104b2
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 12:00:57 +0200

    🛠️ apollo.config.ts -> Updated ApolloClient URI and added credentials and headers.

--- application/src/utils/apollo.config.ts
+++ application/src/utils/apollo.config.ts
@@ -1,5 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const nooCashClient = new ApolloClient({
-    uri: import.meta.env.VITE_GRAPHQL_URL,
-    cache: new InMemoryCache()
+    uri: import.meta.env.VITE_NOOCASH_HASURA_API_URL,
+    cache: new InMemoryCache(),
+    credentials: 'include',
+    headers: {
+        'x-hasura-admin-secret': import.meta.env.VITE_NOOCASH_HASURA_ADMIN_SECRET
+    }
 })
