129dcd5670586f1b39b6108ac6fc0d90e0a7780d
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 23:42:48 +0200

    🟢 subapps/vasha/routes/home/__init__.py - New file for handling home route in Flask app.

---
+++ subapps/vasha/routes/home/__init__.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template,request
+from datetime import date
+
+home = Blueprint('home', __name__)
+
+@home.route('/')
+def index():
+    data = {'title': "noo.cash | apps platform", 'year': date.today().year }
+    return render_template('index.html', data=data)
