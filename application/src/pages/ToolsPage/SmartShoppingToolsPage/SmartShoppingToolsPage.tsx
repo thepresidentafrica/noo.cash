dee730530609761ae93fda119a671a060e17a5f3
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 09:50:01 +0200

    🛠️ SmartShoppingToolsPage.tsx -> Removed NavLink import

--- application/src/pages/ToolsPage/SmartShoppingToolsPage/SmartShoppingToolsPage.tsx
+++ application/src/pages/ToolsPage/SmartShoppingToolsPage/SmartShoppingToolsPage.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { TOOLS_DATA } from "./ToolsData"
 import { PartnersComponent } from "../../../components"
 
