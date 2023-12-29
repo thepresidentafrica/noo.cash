a110cf803f7b5307c1691da0e3998dec606c5fa1
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 23:55:38 +0200

    🟢 subapps/vasha/routes/apps/__init__.py

---
+++ subapps/vasha/routes/apps/__init__.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template,request
+from datetime import date
+
+apps = Blueprint('apps', __name__)
+
+@apps.route('/apps/<app_name>')
+def index(app_name):
+    data = {'title': "noo.cash | apps platform", 'app_name': app_name,  'year': date.today().year }
+    return render_template('apps.html', data=data)
