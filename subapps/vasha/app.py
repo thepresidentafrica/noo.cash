a3170087ad525a4491ad4b1440793b611daf94ab
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-29 23:43:20 +0200

    🛠️ app.py -> Added import for home route, registered home blueprint

--- subapps/vasha/app.py
+++ subapps/vasha/app.py
@@ -4,16 +4,13 @@ import requests
 import os
 from flask import Flask, render_template,request
 from dotenv import load_dotenv
+from routes.home import home
 app = Flask(__name__,static_url_path='/static', static_folder='static')
 app.config['SERVER_NAME'] = None
 app.config['DEBUG'] = True
 
 graphql_url = os.getenv('HASURA_GRAPHQL_ENDPOINT')
 load_dotenv()
-
-@app.route('/')
-def index():
-    return render_template('index.html', env_vars=os.environ)
-
+app.register_blueprint(home)
 if __name__ == '__main__':
     app.run(debug=True)
