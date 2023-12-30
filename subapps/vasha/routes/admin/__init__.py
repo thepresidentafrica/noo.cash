42e35d1826aef948dc98235245856305ecb8e21e
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 07:59:38 +0200

    🛠️ admin/__init__.py -> Updated route to use double quotes and formatted code.

--- subapps/vasha/routes/admin/__init__.py
+++ subapps/vasha/routes/admin/__init__.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template, request
 
-admin = Blueprint('admin', __name__)
+admin = Blueprint("admin", __name__)
 
-@admin.route('/admin')
+
+@admin.route("/admin")
 def index():
-    data = {'title': "noo.cash | Admin", 'year': date.today().year }
-    return render_template('admin.html', data=data)
-
+    data = {"title": "noo.cash | Admin", "year": date.today().year}
+    return render_template("admin.html", data=data)
