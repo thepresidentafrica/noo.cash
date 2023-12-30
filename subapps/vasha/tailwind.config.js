2c8a32b8b9926dc13a9d266d6abb15e16ccfea24
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 10:02:32 +0200

    🛠️ tailwind.config.js -> Added custom color 'nocash'

--- subapps/vasha/tailwind.config.js
+++ subapps/vasha/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./templates/*.{html,js,css}", "./templates/**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'nocash': {  DEFAULT: '#AD13AF', 20: '#FEF6FE', 50: '#FAD1FA',  100: '#F7BFF8',  200: '#F39AF4',  300: '#EE75F0',  400: '#EA50EC',  500: '#E62CE8',  600: '#D117D4',  700: '#AD13AF',  800: '#7B0E7C',  900: '#49084A',  950: '#300530'},
+      }
+    },
   },
   plugins: [],
 }
