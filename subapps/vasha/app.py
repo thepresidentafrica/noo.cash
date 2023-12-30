eb9848d5dec9a446da395ca30ebbbc5ddc2c35dd
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 07:43:00 +0200

    🛠️ app.py -> Updated flask application setup and added admin routes

--- subapps/vasha/app.py
+++ subapps/vasha/app.py
@@ -1,18 +1,16 @@
 
 # create a flask application with html ui
-import requests
 import os
-from flask import Flask, render_template,request
+from flask import Flask
 from dotenv import load_dotenv
-from routes.home import home
-from routes.apps import apps
+from routes import home, apps, admin
 app = Flask(__name__,static_url_path='/static', static_folder='static')
-app.config['SERVER_NAME'] = None
-app.config['DEBUG'] = True
+
 
 graphql_url = os.getenv('HASURA_GRAPHQL_ENDPOINT')
 load_dotenv()
 app.register_blueprint(home)
 app.register_blueprint(apps)
+app.register_blueprint(admin)
 if __name__ == '__main__':
     app.run(debug=True)
