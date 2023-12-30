441b4fe4fcb81bb988ea1f497e24384c7a91af42
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 09:51:05 +0200

    🔴 app.py

--- subapps/vasha/app.py
+++ subapps/vasha/app.py
@@ -5,11 +5,13 @@ from dotenv import load_dotenv
 from routes.home import home
 from routes.apps import apps
 from routes.admin import admin
+from routes.legal import legal
 
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 load_dotenv()
 app.register_blueprint(home)
 app.register_blueprint(apps)
+app.register_blueprint(legal)
 app.register_blueprint(admin)
 if __name__ == "__main__":
     app.run()
