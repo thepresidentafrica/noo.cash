b5304fc22b33c73a6a2d0fda9858668595a039cf
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 07:59:57 +0200

    🛠️ app.py -> Updated flask application with new route imports and removed unused code.

--- subapps/vasha/app.py
+++ subapps/vasha/app.py
@@ -1,16 +1,15 @@
-
 # create a flask application with html ui
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from routes import home, apps, admin
-app = Flask(__name__,static_url_path='/static', static_folder='static')
+from routes.home import home
+from routes.apps import apps
+from routes.admin import admin
 
-
-graphql_url = os.getenv('HASURA_GRAPHQL_ENDPOINT')
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 load_dotenv()
 app.register_blueprint(home)
 app.register_blueprint(apps)
 app.register_blueprint(admin)
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
