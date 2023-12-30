c736ba66033b27dc116977ec61afbc4f6da274b1
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2023-12-30 07:59:51 +0200

    🛠️ home/__init__.py -> Modified import statements and route definition.

--- subapps/vasha/routes/home/__init__.py
+++ subapps/vasha/routes/home/__init__.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template,request
+from flask import Blueprint, render_template, request
 from datetime import date
 
-home = Blueprint('home', __name__)
+home = Blueprint("home", __name__)
 
-@home.route('/j')
+
+@home.route("/")
 def index():
-    data = {'title': "noo.cash | apps platform", 'year': date.today().year }
-    return render_template('index.html', data=data)
+    data = {"title": "noo.cash | apps platform", "year": date.today().year}
+    return render_template("index.html", data=data)
